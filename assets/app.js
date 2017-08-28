8b2204b5f4e5d868633f825d21e6ae0cca14d975
shaunsnowden <ssnowden07@gmail.com>
2017-08-27 23:41:02 -0600

    Working buttons load

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,70 @@
 $(document).ready(function(){
-	var buttons = ["lol", "fml", "Winter is Coming", "teacup pig", "uhhh", "Game of Thrones", "no"];
+
+// Create buttons
+	var buttons = ["LOL", "FML", "OhNo", "TeacupPig", "Uhhh", "GameOfThrones", "No"];
 
 	for (var i=0;i<buttons.length;i++){
 	 	var giphyBtn = $("<button>");
-	 	giphyBtn.addClass("btn btn-primary giphy-button giphy giphy-button-color");
+	 	giphyBtn.addClass("btn btn-primary giphy-button");
 	 	giphyBtn.attr("giphy-tag", buttons[i]);
 	 	giphyBtn.text(buttons[i]);
-	 	$("#gif-container").append(giphyBtn);
+	 	$("#button-container").append(giphyBtn);
 	 };
 
+// Query API after pressing a button
+	$(".giphy-button").on('click',function(){
+
+		$('#gif-container').empty();
+		
+		var gifTag = $(this).text();
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifTag + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+		// console.log(queryURL);
+
+		$.ajax({
+			url: queryURL,
+			method: 'GET'
+
+		}) .done(function(response){
+
+			// console.log(response);
+			var results = response.data;
+
+			for (var i = 0; i < results.length; i++) {
+
+				var gifDiv = $('<div/>');
+				gifDiv.addClass("gifResults");
+
+				var p = $('<p/>');
+				p.text(results[i].rating);
+				console.log(p);
+
+				var gifImage = $('<img/>');
+				gifImage.addClass('gifImageResults');
+				gifImage.attr('src', results[i].images.fixed_height.url);
+				gifImage.attr('data-still', results[i].images.fixed_height.url);
+				gifImage.attr('data-animate', results[i].images.fixed_height.url);
+				// .attr('data-state', 'still');
+				// console.log(gifImage);
+
+
+				gifDiv.append(p);
+				gifDiv.append(gifImage);
+				gifDiv.prependTo($('#gif-container'));
+
+				// console.log(gifImage);
+
+
+			};
+		});
+	});
+
+
+// Submit Button
 	$('#newGifBtn').on('click', function(evt){
 		evt.preventDefault();
 		console.log("Clicked!");
+		// var newTag = 
 	});
 
 });
